298bfc760e5541e4499832771541f1678a5da3f2
Yoga Hanggara <yogahanggara@gmail.com>
2013-11-04 16:10:33 +0700

    add footer ulir

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
 <footer>
 	<div class="container">
+        <div class="row copyright">
+            <div class="col-xs-12">
+                &copy; <?php echo date('Y') ?> by Java Wooden Handicraft
+            </div>
+        </div>
+        <div class="row ulir">
 
+        </div>
 	</div>
 </footer>
 
