bd4d955455ca27191f5ab01fae93beef9c386905
root <root@localhost.localdomain>
2013-11-25 06:16:37 +0700

    thumb in homepage

--- home.php
+++ home.php
@@ -70,9 +70,13 @@ foreach($myposts as $post):
 
     }
 
+    $img_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'product-thumb');
+
 ?>
                     <div class="item">
-                        <a href="<?php echo $post_category_link ?>"><img src="<?php echo get_template_directory_uri() ?>/timthumb.php?src=img/dummy/photo-<?php echo rand(1,12) ?>.jpg&amp;w=300&amp;h=300" class="img-responsive" /></a>
+                        <a href="<?php echo $post_category_link ?>">
+                            <img src="<?php echo $img_thumb ?>" class="img-responsive" />
+                        </a>
                         <div class="meta-product">
                             <div class="row">
                                 <div class="col-sm-6 meta-product-title">
